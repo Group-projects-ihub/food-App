31f4218605cec4521e0e73145f313611d835d375
karanidenis <d.waweru@alustudent.com>
2023-06-06 17:56:42 +0300

    updates

--- Backend/models/restaurants.py
+++ Backend/models/restaurants.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, VARCHAR
 
 from models.basemodel import BaseModel, Base
 
@@ -7,18 +7,17 @@ from models.basemodel import BaseModel, Base
 class Restaurant(BaseModel, Base):
     """Model class for a restaurant"""
 
+    __tablename__ = "restaurants"
     name = Column(String(255), nullable=False)
     address = Column(String(255), nullable=False)
-    cuisine = Column(String(255), nullable=False)
+    description = Column(VARCHAR(255), nullable=False)
+    rating = Column(VARCHAR(255), nullable=False)
 
-    def __init__(self, name, address, cuisine, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = name
-        self.address = address
-        self.cuisine = cuisine
 
-    def __str__(self):
-        return "[{:s}] ({:s}) Name: {:s}, Address: {:s}, Cuisine: {:s}".format(
-            self.__class__.__name__, self.id, self.name, self.address, self.cuisine
-        )
-
+    # def __str__(self):
+    #     return "[{:s}] ({:s}) Name: {:s}, Address: {:s}, Cuisine: {:s}".format(
+    #         self.__class__.__name__, self.id, self.name, self.address, self.cuisine
+    #     )
